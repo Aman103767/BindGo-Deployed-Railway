44b81c9e7fcbe65124de31f5d1d43422cb23d4a8
unknown <avinash103767@gmail.com>
2023-09-03 13:14:23 +0530

    changes for railways deployement

--- E-Commerce-Application/src/main/java/com/hb/ECommerceApplication.java
+++ E-Commerce-Application/src/main/java/com/hb/ECommerceApplication.java
@@ -6,7 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		}
+)
 @SpringBootApplication
 @EnableSwagger2
 public class ECommerceApplication {
